0ec66fbb4e174a3ca491d5c54ddb2398cd9669af
Limuran <strelgl2@gmail.com>
2023-11-04 23:13:27 +1000

    Redesigned info about training images
    Moved all info (corners, figures, directions) to one file true_boards.txt
    Created functions to parse this file

--- ShogiNeuralNetwork/data_info.py
+++ ShogiNeuralNetwork/data_info.py
@@ -2,36 +2,6 @@ from extra.figures import Figure, Direction
 from config import Paths
 import os
 
-IMGS_CORNERS = {
-    "board1.jpg": ((425, 759), (2871, 725), (2981, 3508), (381, 3522)),
-    "board2.jpg": ((570, 1036), (2759, 992), (2962, 3526), (442, 3561)),
-    "board3.jpg": ((809, 793), (2829, 1214), (2700, 3340), (821, 3859)),
-    "board4.jpg": ((826, 499), (2602, 1104), (2590, 3132), (755, 3366)),
-    "board5.jpg": ((920, 2002), (2620, 1930), (3289, 3676), (319, 3733)),
-    "board6.jpg": ((49, 109), (491, 105), (510, 593), (52, 603)),
-    "board7.jpg": ((68, 53), (547, 55), (558, 592), (52, 589)),
-    "board8.jpg": ((61, 61), (475, 50), (504, 508), (66, 517)),
-    "board9.jpg": ((51, 48), (499, 43), (520, 534), (61, 551)),
-    "board10.jpg": ((68, 69), (456, 65), (494, 484), (79, 506)),
-    "board11.jpg": ((50, 48), (491, 45), (507, 531), (52, 540)),
-    "board12.jpg": ((35, 47), (493, 44), (512, 544), (45, 558)),
-    "board13.jpg": ((1583, 980), (3421, 956), (3462, 2806), (1608, 2827)),
-    "board14.jpg": ((1594, 976), (3429, 967), (3454, 2814), (1602, 2822)),
-    "board15.jpg": ((510, 1031), (2723, 981), (2824, 3759), (445, 3756)),
-    "board16.jpg": ((633, 1282), (2585, 1218), (2684, 3598), (676, 3653)),
-    "board17.jpg": ((463, 1370), (2610, 1341), (2668, 3966), (461, 3988)),
-    "board18.jpg": ((642, 1128), (2692, 1086), (2761, 3624), (620, 3620)),
-    "board19.jpg": ((647, 1491), (2571, 1499), (2629, 3863), (602, 3888)),
-    "board20.jpg": ((670, 1295), (2553, 1383), (2467, 3659), (558, 3581)),
-    "board21.jpg": ((431, 1281), (2606, 1232), (2653, 3880), (463, 3881)),
-    "board22.jpg": ((412, 872), (2825, 884), (2841, 3814), (370, 3810)),
-    "board23.jpg": ((602, 1087), (2906, 1064), (2932, 4017), (458, 3901)),
-    "board24.jpg": ((372, 842), (2847, 783), (2934, 3863), (331, 3856)),
-    "board25.jpg": ((574, 1166), (2858, 1155), (2932, 3999), (503, 3987)),
-    "board26.jpg": ((721, 1182), (2680, 1219), (2675, 3674), (572, 3588)),
-    "board27.jpg": ((635, 1438), (2631, 1526), (2605, 4029), (449, 3948)),
-}
-
 IMGS = [
     "board1.jpg",
     "board2.jpg",
@@ -60,13 +30,48 @@ IMGS = [
     "board25.jpg",
     "board26.jpg",
     "board27.jpg",
+    "board28.jpg",
+    "board29.jpg",
+    "board30.jpg",
+    "board31.jpg",
+    "board32.jpg",
+    "board33.jpg",
+    "board34.jpg",
+    "board35.jpg",
+    "board36.jpg",
+    "board37.jpg",
+    "board38.jpg",
+    "board39.jpg",
+    "board40.jpg",
+    "board41.jpg",
+    "board42.jpg",
+    "board43.jpg",
+    "board44.jpg",
+    "board45.jpg",
+    "board46.jpg",
+    "board47.jpg",
+    "board48.jpg",
+    "board49.jpg",
+    "board50.jpg",
+    "board51.jpg",
+    "board52.jpg",
+    "board53.jpg",
+    "board54.jpg",
+    "board55.jpg",
+    "board56.jpg",
+    "board57.jpg",
+    "board58.jpg",
+    "board59.jpg",
+    "board60.jpg",
+    "board61.jpg",
+    "board62.jpg",
+    "board63.jpg",
+    "board64.jpg",
+    "board65.jpg",
+    "board66.jpg",
+    "board67.jpg",
+    "board68.jpg",
 ]
-
-for path in list(IMGS_CORNERS.keys()):
-    abs_path = os.path.join(Paths.TRAIN_BOARDS_DIR, path)
-    IMGS_CORNERS[abs_path] = IMGS_CORNERS[path]
-for i, path in enumerate(IMGS):
-    IMGS[i] = os.path.join(Paths.TRAIN_BOARDS_DIR, path)
 
 CATEGORIES_FIGURE_TYPE = [Figure.PAWN, Figure.BISHOP, Figure.ROOK, Figure.LANCE,
                           Figure.KNIGHT, Figure.SILVER, Figure.GOLD, Figure.KING, Figure.EMPTY]
--- ShogiNeuralNetwork/true_boards.py
+++ ShogiNeuralNetwork/true_boards.py
@@ -1,508 +1,57 @@
-from config.Paths import TRAIN_BOARDS_DIR
-import os
+from extra.types import FigureBoard, DirectionBoard, Corners
+from extra.figures import Figure, Direction
+from dataclasses import dataclass
 
 
-class TrueFigureTypes:
-    START_BOARD = """
-        LNSGKGSNL
-        .R.....B.
-        PPPPPPPPP
-        .........
-        .........
-        .........
-        PPPPPPPPP
-        .B.....R.
-        LNSGKGSNL
-    """
-
-    TRUE_BOARDS = {
-        "board1.jpg": START_BOARD,
-        "board2.jpg": START_BOARD,
-        "board3.jpg": START_BOARD,
-        "board4.jpg": START_BOARD,
-        "board5.jpg": START_BOARD,
-        "board6.jpg": """
-            LNKG.GSNL
-            .RS....B.
-            PPPPPP.PP
-            ......P..
-            .......P.
-            ..P.P..R.
-            PP.P.PP.P
-            .B...K...
-            LNSG.GSNL
-        """,
-        "board7.jpg": """
-            LNKG.GSNL
-            .RS....B.
-            PPPPPP.PP
-            ......P..
-            .......P.
-            ..P....R.
-            PP.PPPP.P
-            .B...K...
-            LNSG.GSNL
-        """,
-        "board8.jpg": """
-            LNKG.GSNL
-            .RS....B.
-            .PPPPP.PP
-            P.....P..
-            ....B..P.
-            ..P.P..R.
-            PP.P.PP.P
-            .....K...
-            LNSG.GSNL
-        """,
-        "board9.jpg": """
-            LNKG.GSNL
-            .RS....B.
-            .PPPPP.PP
-            P.....P..
-            ....B..P.
-            ..P.P..R.
-            PP.P.PP.P
-            .....K...
-            LNSG.GSNL
-        """,
-        "board10.jpg": """
-            LNKG.GSNL
-            .RS....B.
-            PPPPPP.PP
-            ......P..
-            .......P.
-            ..P....R.
-            PP.PPPP.P
-            .B...K...
-            LNSG.GSNL
-        """,
-        "board11.jpg": """
-            LNKG.GSNL
-            .RS....B.
-            PPPPPP.PP
-            ......P..
-            .......P.
-            ..P.P..R.
-            PP.P.PP.P
-            .B...K...
-            LNSG.GSNL
-        """,
-        "board12.jpg": """
-            LNKG.GSNL
-            .RS....B.
-            PPPPPP.PP
-            ......P..
-            ....B..P.
-            ..P.P..R.
-            PP.P.PP.P
-            .....K...
-            LNSG.GSNL
-        """,
-        "board13.jpg": """
-            ....L....
-            ..S...R..
-            ....N...L
-            .P..N..G.
-            .........
-            ..B.P..S.
-            .........
-            ..K....G.
-            .....P...
-        """,
-
-        "board14.jpg": """
-            ......L..
-            .........
-            ....NL...
-            ...SN.R..
-            ...P.....
-            .........
-            ...PG....
-            .B..KSG..
-            .....P...
-        """,
-
-        "board15.jpg": START_BOARD,
-        "board16.jpg": START_BOARD,
-        "board17.jpg": """
-            LN.....NL
-            ..G.KSG..
-            P.PPS..PP
-            ....PPB..
-            .P....P..
-            ..P..P.P.
-            PPSP..P.P
-            ...GRKS..
-            LN...G.NL
-        """,
-        "board18.jpg": """
-            .....RNNL
-            ....BSG..
-            ......K.P
-            ..PPRPBP.
-            .P....P..
-            ..PS.P.P.
-            .P.P..P.P
-            ...G.KS..
-            .L...G.NL
-        """,
-        "board19.jpg": """
-            .....RNNL
-            ....BSG..
-            ......K.P
-            ..PPRPBP.
-            .P....P..
-            ..PS.P.P.
-            .P.P..P.P
-            ...G.KS..
-            .L...G.NL
-        """,
-        "board20.jpg": """
-            ......KNL
-            L.G.RSG..
-            N.PP..SPP
-            P.P.BPP..
-            .........
-            ...B.....
-            P.NPPPPPP
-            ..G...S..
-            L.S..GKNL
-        """,
-        "board21.jpg": """
-            ........L
-            .....S...
-            .......RP
-            ..PP.P...
-            .P....P.K
-            ..PS.P.PP
-            .P.P..P..
-            ...G.KS..
-            .L...G.NL
-        """,
-        "board22.jpg": """
-            LN..K..NL
-            ...S.SG..
-            P.PBPPPP.
-            ..R.....P
-            .P.......
-            ..PR.....
-            PPB.PPPP.
-            ....GKS..
-            ..S..G.NL
-        """,
-        "board23.jpg": """
-            LN..K....
-            ..GS.SG..
-            P.P.PPNPB
-            ...P..P..
-            .R.L.....
-            ..P......
-            PPSPPPPPP
-            ..G.RG...
-            LB..K.SNL
-        """,
-        "board24.jpg": """
-            LN..K..NL
-            .....SG..
-            P.PRPPPP.
-            ..R.....P
-            .P.......
-            ..P......
-            PPB.PPPP.
-            ....GKS..
-            ..S..G.NL
-        """,
-        "board25.jpg": """
-            LN..K..NL
-            .....SG..
-            P.PRPPPP.
-            ..R.....P
-            .P.......
-            ..P......
-            PPB.PPPP.
-            ....BKS..
-            ..S..G.NL
-        """,
-        "board26.jpg": """
-            LN.......
-            ..GSKSG..
-            P.PNPPNPB
-            ...P..P..
-            .B.......
-            ..P.P....
-            PP.L.PPPP
-            .R...G...
-            LB..K.SKL
-        """,
-        "board27.jpg": """
-            LNG......
-            ....KSG..
-            P...PP.P.
-            ..PP..P..
-            .....N...
-            ..PNP....
-            PRG.KPPPP
-            ..SB.....
-            L....GSNL
-        """
-    }
+@dataclass(frozen=True)
+class BoardPhotoInfo:
+    img_name: str
+    figures: FigureBoard
+    directions: DirectionBoard
+    corners: Corners
 
 
-class TrueDirections:
-    """Направление фигур. U - смотрят вверх, D - смотрят вниз"""
+# Loading file with info
+__boards: dict[str, BoardPhotoInfo] = dict()
+with open("true_boards.txt", "r") as f:
+    while line := f.readline():
+        if line.strip() == "":
+            continue
+        else:
+            im_name = line.strip()
 
-    START_BOARD = """
-    DDDDDDDDD
-    .D.....D.
-    DDDDDDDDD
-    .........
-    .........
-    .........
-    UUUUUUUUU
-    .U.....U.
-    UUUUUUUUU
-    """
+            corners = []
+            for _ in range(4):
+                corners.append([int(i) for i in f.readline().strip().split()])
+            f.readline()
 
-    TRUE_BOARDS = {
-        "board1.jpg": START_BOARD,
-        "board2.jpg": START_BOARD,
-        "board3.jpg": START_BOARD,
-        "board4.jpg": START_BOARD,
-        "board5.jpg": START_BOARD,
-        "board6.jpg": """
-            DDDD.DDDD
-            .DD....D.
-            DDDDDD.DD
-            ......D..
-            .......U.
-            ..U.U..U.
-            UU.U.UU.U
-            .U...U...
-            UUUU.UUUU
-        """,
-        "board7.jpg": """
-            DDDD.DDDD
-            .DD....D.
-            DDDDDD.DD
-            ......D..
-            .......U.
-            ..U....U.
-            UU.UUUU.U
-            .U...U...
-            UUUU.UUUU
-        """,
-        "board8.jpg": """
-            DDDD.DDDD
-            .DD....D.
-            .DDDDD.DD
-            D.....D..
-            ....U..U.
-            ..U.U..U.
-            UU.U.UU.U
-            .....U...
-            UUUU.UUUU
-        """,
-        "board9.jpg": """
-            DDDD.DDDD
-            .DD....D.
-            .DDDDD.DD
-            D.....D..
-            ....U..U.
-            ..U.U..U.
-            UU.U.UU.U
-            .....U...
-            UUUU.UUUU
-        """,
-        "board10.jpg": """
-            DDDD.DDDD
-            .DD....D.
-            DDDDDD.DD
-            ......D..
-            .......U.
-            ..U....U.
-            UU.UUUU.U
-            .U...U...
-            UUUU.UUUU
-        """,
-        "board11.jpg": """
-            DDDD.DDDD
-            .DD....D.
-            DDDDDD.DD
-            ......D..
-            .......U.
-            ..U.U..U.
-            UU.U.UU.U
-            .U...U...
-            UUUU.UUUU
-        """,
-        "board12.jpg": """
-            DDDD.DDDD
-            .DD....D.
-            DDDDDD.DD
-            ......D..
-            ....U..U.
-            ..U.U..U.
-            UU.U.UU.U
-            .....U...
-            UUUU.UUUU
-        """,
-        "board13.jpg": """
-            ....D....
-            ..U...D..
-            ....U...U
-            .U..D..U.
-            .........
-            ..U.U..D.
-            .........
-            ..U....U.
-            .....U...
-        """,
+            figures = []
+            for _ in range(9):
+                fig_row = [Figure(char) for char in f.readline().strip()]
+                figures.append(fig_row)
+            f.readline()
 
-        "board14.jpg": """
-            ......D..
-            .........
-            ....UD...
-            ...UD.D..
-            ...U.....
-            .........
-            ...UU....
-            .U..UDU..
-            .....U...
-        """,
+            directions = []
+            for _ in range(9):
+                directions_row = [Direction(char) for char in f.readline().strip()]
+                directions.append(directions_row)
+            f.readline()
 
-        "board15.jpg": START_BOARD,
-        "board16.jpg": START_BOARD,
-        "board17.jpg": """
-            DD.....DD
-            ..D.DDD..
-            D.DDD..DD
-            ....DDD..
-            .D....D..
-            ..U..U.U.
-            UUUU..U.U
-            ...UUUU..
-            UU...U.UU
-        """,
-        "board18.jpg": """
-            .....UDDD
-            ....UDD..
-            ......D.D
-            ..DDUDDD.
-            .D....D..
-            ..UU.U.U.
-            .U.U..U.U
-            ...U.UU..
-            .D...U.UU
-        """,
-        "board19.jpg": """
-            .....UDDD
-            ....UDD..
-            ......D.D
-            ..DDUDDD.
-            .D....D..
-            ..UU.U.U.
-            .U.U..U.U
-            ...U.UU..
-            .D...U.UU
-        """,
-        "board20.jpg": """
-            ......DDD
-            D.D.UDD..
-            D.DD..DDD
-            D.U.UDD..
-            .........
-            ...U.....
-            U.UUUUUUU
-            ..U...U..
-            U.U..UUUU
-        """,
-        "board21.jpg": """
-            ........D
-            .....U...
-            .......UD
-            ..DD.D...
-            .D....D.D
-            ..UU.U.UU
-            .U.U..U..
-            ...U.UU..
-            .D...U.UU
-        """,
-        "board22.jpg": """
-            DD..D..DD
-            ...D.DD..
-            D.DUDDDD.
-            ..D.....U
-            .D.......
-            ..UU.....
-            UUD.UUUU.
-            ....UUU..
-            ..U..U.UU
-        """,
-        "board23.jpg": """
-            DD..D....
-            ..DD.DD..
-            D.D.DDDDU
-            ...U..D..
-            .D.D.....
-            ..U......
-            UUUDUUUUU
-            ..U.UU...
-            UD..U.UUU
-        """,
-        "board24.jpg": """
-            DD..D..DD
-            .....DD..
-            D.DUDDDD.
-            ..D.....U
-            .D.......
-            ..U......
-            UUD.UUUU.
-            ....UUU..
-            ..U..U.UU
-        """,
-        "board25.jpg": """
-            DD..D..DD
-            .....DD..
-            D.DUDDDD.
-            ..D.....U
-            .D.......
-            ..U......
-            UUD.UUUU.
-            ....UUU..
-            ..U..U.UU
-        """,
-        "board26.jpg": """
-            DD.......
-            ..DDDDD..
-            D.DUDDDDU
-            ...U..D..
-            .D.......
-            ..U.U....
-            UU.D.UUUU
-            .U...U...
-            UD..U.UUU
-        """,
-        "board27.jpg": """
-            DDD......
-            ....DDD..
-            D...DD.D.
-            ..DU..D..
-            .....D...
-            ..UDU....
-            UDU.UUUUU
-            ..DU.....
-            U....DUUU
-        """
-    }
+            __boards[im_name] = BoardPhotoInfo(
+                img_name=im_name,
+                figures=figures,
+                directions=directions,
+                corners=corners
+            )
 
 
-for path in list(TrueFigureTypes.TRUE_BOARDS.keys()):
-    board = TrueFigureTypes.TRUE_BOARDS[path].strip().replace(" ", "").split("\n")
-    TrueFigureTypes.TRUE_BOARDS[path] = board
-    abs_path = os.path.join(TRAIN_BOARDS_DIR, path)
-    TrueFigureTypes.TRUE_BOARDS[abs_path] = board
-for path in list(TrueDirections.TRUE_BOARDS.keys()):
-    board = TrueDirections.TRUE_BOARDS[path].strip().replace(" ", "").split("\n")
-    TrueDirections.TRUE_BOARDS[path] = board
-    abs_path = os.path.join(TRAIN_BOARDS_DIR, path)
-    TrueDirections.TRUE_BOARDS[abs_path] = board
+def get_true_figures(img_name: str) -> FigureBoard:
+    return __boards[img_name].figures
+
+
+def get_true_directions(img_name: str) -> DirectionBoard:
+    return __boards[img_name].directions
+
+
+def get_corners(img_name: str) -> Corners:
+    return __boards[img_name].corners
