24d860932842eb02e2acceebea732bb8814a89c0
Limuran <strelgl2@gmail.com>
2023-11-04 23:23:41 +1000

    Convenience changes
    Moved image mode to one constant
    Added image mode options to functions
    Added factories for video and photos
    
    
    Former-commit-id: 4d82ee63d494a60ebb0565eb825b42998e4c215b

--- extra/factories.py
+++ extra/factories.py
@@ -1,9 +1,12 @@
+import os.path
 from Elements import *
 import numpy as np
 from config import Config, Paths
+from ShogiNeuralNetwork import data_info
+from extra.image_modes import ImageMode
 
 
-def get_camera_reader(cam_id: int):
+def get_camera_reader(image_mode: ImageMode, cam_id: int):
     config = Config(Paths.CONFIG_PATH)
     hsv_low = np.array([
         config.HSVThreshold.h_low,
@@ -17,8 +20,63 @@ def get_camera_reader(cam_id: int):
     ])
 
     reader = ShogiBoardReader(
+        image_mode,
         BoardSplitter.BoardSplitter(
             ImageGetters.Camera(cam_id),
+            CornerGetters.HSVThresholdCornerDetector(hsv_low, hsv_high),
+            board_img_size=config.NN_data.board_img_size,
+            cell_img_size=config.NN_data.cell_img_size
+        ),
+        FigureRecognizers.RecognizerNN(
+            Paths.MODEL_FIGURE_PATH,
+            Paths.MODEL_DIRECTION_PATH,
+            cell_img_size=config.NN_data.cell_img_size
+        ),
+        analyzer=None
+    )
+    return reader
+
+
+def get_hardcoded_reader(image_mode: ImageMode, img_name: str):
+    config = Config(Paths.CONFIG_PATH)
+    img_path = os.path.join(Paths.TRAIN_BOARDS_DIR, img_name)
+    corners = data_info.IMGS_CORNERS[img_path]
+
+    reader = ShogiBoardReader(
+        image_mode,
+        BoardSplitter.BoardSplitter(
+            ImageGetters.Photo(img_path),
+            CornerGetters.HardcodedCornerDetector(corners),
+            board_img_size=config.NN_data.board_img_size,
+            cell_img_size=config.NN_data.cell_img_size
+        ),
+        FigureRecognizers.RecognizerNN(
+            Paths.MODEL_FIGURE_PATH,
+            Paths.MODEL_DIRECTION_PATH,
+            cell_img_size=config.NN_data.cell_img_size
+        ),
+        analyzer=None
+    )
+    return reader
+
+
+def get_video_reader(image_mode: ImageMode, video_path: str):
+    config = Config(Paths.CONFIG_PATH)
+    hsv_low = np.array([
+        config.HSVThreshold.h_low,
+        config.HSVThreshold.s_low,
+        config.HSVThreshold.v_low,
+    ])
+    hsv_high = np.array([
+        config.HSVThreshold.h_high,
+        config.HSVThreshold.s_high,
+        config.HSVThreshold.v_high,
+    ])
+
+    reader = ShogiBoardReader(
+        image_mode,
+        BoardSplitter.BoardSplitter(
+            ImageGetters.Video(video_path),
             CornerGetters.HSVThresholdCornerDetector(hsv_low, hsv_high),
             board_img_size=config.NN_data.board_img_size,
             cell_img_size=config.NN_data.cell_img_size
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from extra import factories
 import cv2
+from extra import utils
+from extra.image_modes import ImageMode
+
+
+IMAGE_MODE = ImageMode.GRAYSCALE
 
 
 def camera():
-    reader = factories.get_camera_reader(cam_id=0)
+    reader = factories.get_camera_reader(IMAGE_MODE, cam_id=0)
     size = 700
 
     while True:
@@ -28,11 +33,11 @@ def camera():
 
 
 def photo():
-    reader = factories.get_hardcoded_reader()
+    reader = factories.get_hardcoded_reader(IMAGE_MODE, "board20.jpg")
     board_img = reader.get_board_image_no_perspective()
     predicted_board_img = reader.get_digital_board()
-    board_img = cv2.resize(board_img, (800, 800))
-    predicted_board_img = cv2.resize(predicted_board_img, (800, 800))
+    board_img = cv2.resize(board_img, (600, 600))
+    predicted_board_img = cv2.resize(predicted_board_img, (600, 600))
     img = cv2.hconcat((board_img, predicted_board_img))
 
     cv2.imshow("", img)
@@ -41,21 +46,26 @@ def photo():
 
 def video():
     reader = factories.get_video_reader(
-        "temp/img/VID_20230731_160231.mp4"
+        IMAGE_MODE,
+        "temp/VID_20231103_163606.mp4"
     )
 
     while True:
-        board_img = reader.get_full_img_with_borders()
+        board_img = reader.get_full_img(show_borders=True)
         predicted_board_img = reader.get_digital_board()
 
-        no_persp = reader.get_board_image_no_perspective()
+        no_persp = reader.get_board_image_no_perspective(img_mode=ImageMode.GRAYSCALE_BLACK_THRESHOLD)
+        no_persp = utils.gray_to_3d(no_persp)
 
-        board_img = cv2.resize(board_img, (800, 800))
-        predicted_board_img = cv2.resize(predicted_board_img, (800, 800))
-        no_persp = cv2.resize(no_persp, (800, 800))
+        no_persp = reader.get_board_image_no_perspective()
+        # no_persp = reader.get_board_image_no_perspective()
+
+        board_img = cv2.resize(board_img, (600, 600))
+        predicted_board_img = cv2.resize(predicted_board_img, (600, 600))
+        no_persp = cv2.resize(no_persp, (600, 600))
         img = cv2.hconcat((
-            board_img,
-            # no_persp,
+            # board_img,
+            no_persp,
             predicted_board_img,
         ))
 
@@ -65,8 +75,6 @@ def video():
     cv2.destroyAllWindows()
 
 
-camera()
+# camera()
 # photo()
-# video()
-
-
+video()
