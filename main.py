dfbee00b5a448a846b2f7bf061d2db282b89fd9b
Limuran <strelgl2@gmail.com>
2023-11-15 00:39:20 +1000

    Moved files
    Removed analysis because it does not work anymore
    Moved GUI main to root
    Moved create_models to root
    
    
    Former-commit-id: 75fd502c27d1c77d57ca00e53279a3af5305eaf3

--- main.py
+++ main.py
@@ -1,92 +1,12 @@
-from extra import factories
-import cv2
-from extra import utils
-from extra.image_modes import ImageMode
-from config import GLOBAL_CONFIG
+from PyQt5.QtWidgets import QApplication
+from GUI.view import View
+import os
+
+os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
 
 
-def camera():
-    reader = factories.get_camera_reader(
-        ImageMode(GLOBAL_CONFIG.NeuralNetwork.image_mode),
-        cam_id=0
-    )
-    size = 700
-
-    while True:
-        board_img = reader.get_full_img(show_borders=True)
-        predicted_board_img = reader.get_digital_board()
-        no_persp = reader.get_board_image_no_perspective()
-
-        board_img = cv2.resize(board_img, (size, size))
-        predicted_board_img = cv2.resize(predicted_board_img, (size, size))
-        no_persp = cv2.resize(no_persp, (size, size))
-
-        img = cv2.hconcat((
-            board_img,
-            # no_persp,
-            predicted_board_img,
-        ))
-
-        cv2.imshow("", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cv2.destroyAllWindows()
-
-
-def photo():
-    reader = factories.get_hardcoded_reader(
-        ImageMode(GLOBAL_CONFIG.NeuralNetwork.image_mode),
-        "board20.jpg"
-    )
-    board_img = reader.get_board_image_no_perspective()
-    predicted_board_img = reader.get_digital_board()
-    board_img = cv2.resize(board_img, (600, 600))
-    predicted_board_img = cv2.resize(predicted_board_img, (600, 600))
-    img = cv2.hconcat((board_img, predicted_board_img))
-
-    cv2.imshow("", img)
-    cv2.waitKey(0)
-
-
-def video():
-    reader = factories.get_video_reader(
-        ImageMode(GLOBAL_CONFIG.NeuralNetwork.image_mode),
-        "/home/gleb/ShogiBoardReader/temp/VID_20231110_170554.mp4",
-        use_memorizer=False
-    )
-    from time import time
-    while True:
-
-        s = time()
-        reader.update()
-        print("FPS:", 1 / (time() - s))
-
-        board_img = reader.get_full_img(show_borders=True)
-        board = reader.get_board()
-        predicted_board_img = board.to_image()
-
-        no_persp = reader.get_board_image_no_perspective(img_mode=ImageMode.GRAYSCALE_BLACK_THRESHOLD)
-        no_persp = utils.gray_to_3d(no_persp)
-
-        no_persp = reader.get_board_image_no_perspective()
-        # no_persp = reader.get_board_image_no_perspective()
-
-        board_img = cv2.resize(board_img, (600, 600))
-        predicted_board_img = cv2.resize(predicted_board_img, (600, 600))
-        no_persp = cv2.resize(no_persp, (600, 600))
-        img = cv2.hconcat((
-            # board_img,
-            no_persp,
-            predicted_board_img,
-        ))
-
-        cv2.imshow("", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            # reader.memorizer.save_to_kifu("/home/gleb/ShogiBoardReader/temp/kifu.kif")
-            break
-    cv2.destroyAllWindows()
-
-
-# camera()
-# photo()
-video()
+if __name__ == "__main__":
+    app = QApplication([])
+    window = View()
+    window.show()
+    app.exec_()
